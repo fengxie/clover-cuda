70360cba63f6e568eda6ba5b812d002343aeec9c
Fung Tse <fengxie83@gmail.com>
2018-12-26 10:35:08 +0800

    Add anti alias support

--- clover_kernel.cu
+++ clover_kernel.cu
@@ -8,33 +8,68 @@
 
 #define CUDART_PI_F 3.141592654f
 
-__global__ void clover_kernel(uchar4 *image, float scale, size_t stride, const dim3 center)
+template<int AA_X_ = 2,
+         int AA_Y_ = 2>
+__global__ void clover_kernel(uchar4 *image, uchar4 *background, float scale, size_t stride, const dim3 center)
 {
     int tx = blockIdx.x * blockDim.x + threadIdx.x;
     int ty = blockIdx.y * blockDim.y + threadIdx.y;
 
     uchar4 *ptr = image + ty * stride + tx;
+    uchar4 *inptr = background + ty * stride + tx;
 
-    float x = tx - (int)center.x;
-    float y = (int)center.y - ty;
+    uchar4 rgba_out = make_uchar4(0, 0, 0, 0);
+    for (int i = 0; i < AA_Y_; i++) {
+        for (int j = 0; j < AA_X_; j++) {
+            float x = tx - (int)center.x + j * 1.0f / AA_X_;
+            float y = (int)center.y - ty + i * 1.0f / AA_Y_;
 
-    float radius = sqrt(x * x + y * y);
-    float theta  = atan2(y, x) + CUDART_PI_F / 6.0;
-    float bound = scale * abs(sin(2.0f * theta) + sin(6.0f * theta) / 4.0f);
+            float radius = sqrt(x * x + y * y);
+            float theta  = atan2(y, x) + CUDART_PI_F / 6.0;
+            float bound = scale * abs(sin(2.0f * theta) + sin(6.0f * theta) / 4.0f);
 
-    uchar4 rgba = make_uchar4(255, 0, 0, 255);
+            uchar4 rgba = make_uchar4(0, 0, 0, 255);
 
-    if (radius < bound && radius > 0.5 * bound) {
-        rgba.x = 0;
-        rgba.y = 255;
-        rgba.z = 0;
-        rgba.w = 255;
-    } else if (radius < 0.5 * bound) {
-        rgba.x = 128;
-        rgba.y = 255;
-        rgba.z = 128;
-        rgba.w = 255;
+            if (radius < bound && radius > 0.5 * bound) {
+                rgba.x = 0;
+                rgba.y = 255;
+                rgba.z = 0;
+                rgba.w = 255;
+            } else if (radius < 0.5 * bound) {
+                rgba.x = 128;
+                rgba.y = 255;
+                rgba.z = 128;
+                rgba.w = 255;
+            }
+
+            rgba_out.x += rgba.x / (AA_X_ * AA_Y_);
+            rgba_out.y += rgba.y / (AA_X_ * AA_Y_);
+            rgba_out.z += rgba.z / (AA_X_ * AA_Y_);
+            rgba_out.w += rgba.w / (AA_X_ * AA_Y_);
+        }
     }
 
-    *ptr = rgba;
+#if 0
+    float f = 0.8;
+    float s = f + (1 - f) * (1.0f - (inptr[0].x + inptr[0].y + inptr[0].z + inptr[0].w) / 1024.0f);
+#else
+    float s = 1.0f;
+#endif
+    rgba_out.x = s * rgba_out.x;
+    rgba_out.y = s * rgba_out.y;
+    rgba_out.z = s * rgba_out.z;
+    rgba_out.w = s * rgba_out.w;
+
+    *ptr = rgba_out;
 }
+
+template __global__ void clover_kernel<2, 2>(uchar4 *image, uchar4 *background, float scale, size_t stride, const dim3 center);
+template __global__ void clover_kernel<4, 4>(uchar4 *image, uchar4 *background, float scale, size_t stride, const dim3 center);
+
+template<int tile_x = 4,
+         int tile_y = 4,
+         int filter_size = 3>
+__global__ void convolve(uchar4 *image, float scale, unsigned char kernel[filter_size][filter_size], size_t stride)
+{
+}
+
--- main.cu
+++ main.cu
@@ -7,36 +7,102 @@
 
 #include "util.h"
 
-__global__ void clover_kernel(uchar4 *image, float scale, size_t stride, const dim3 center);
+template<int AA_X_ = 2,
+         int AA_Y_ = 2>
+__global__ void clover_kernel(uchar4 *image, uchar4 *background, float scale, size_t stride, const dim3 center);
 extern int write_png(const char *filename, int width, int height, unsigned char *buffer, char* title);
+extern int read_png(const char* filename, int width, int height, unsigned char *buffer);
 
 int main(const int argc, const char *argv[])
 {
+    unsigned char *input;
+    unsigned char *device_input;
     unsigned char *output;
     unsigned char *device_output;
 
     const size_t height = 1024;
     const size_t width  = 1024;
     const size_t size   = 4 * height * width;
+    input  = new unsigned char[size];
     output = new unsigned char[size];
 
+    read_png("background.png", width, height, input);
+
+    CUDA_ASSERT(cudaMalloc((void **)&device_input, size * sizeof(unsigned char)), "");
     CUDA_ASSERT(cudaMalloc((void **)&device_output, size * sizeof(unsigned char)), "");
 
+    CUDA_ASSERT(cudaMemcpy(device_input, input, size * sizeof(unsigned char), cudaMemcpyHostToDevice), "");
     CUDA_ASSERT(cudaMemcpy(device_output, output, size * sizeof(unsigned char), cudaMemcpyHostToDevice), "");
 
     dim3 threads(16, 16, 1);
     dim3 blocks(width / threads.x, height / threads.y, 1);
     dim3 center(width / 2, height / 2, 1);
-    clover_kernel<<<blocks, threads>>>((uchar4 *)device_output, 512.0f, width, center);
+    clover_kernel<4, 4><<<blocks, threads>>>((uchar4 *)device_output, (uchar4 *)device_input, 512.0f, width, center);
 
     CUDA_ASSERT(cudaMemcpy(output, device_output, size * sizeof(unsigned char), cudaMemcpyDeviceToHost), "");
     write_png("clover.png", width, height, output, "Clover");
 
+    CUDA_ASSERT(cudaFree(device_input), "");
     CUDA_ASSERT(cudaFree(device_output), "");
 
+    delete[] input;
     delete[] output;
 
     return 0;
+}
+
+int read_png(const char* filename, int width, int height, unsigned char *buffer)
+{
+    int code = 0;
+    FILE *fp = NULL;
+    png_structp png_ptr = NULL;
+
+    // Open file for writing (binary mode)
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Could not open file %s for writing\n", filename);
+        code = 1;
+        goto finalise;
+    }
+
+    // Initialize write structure
+    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL) {
+        fprintf(stderr, "Could not allocate write struct\n");
+        code = 1;
+        goto finalise;
+    }
+
+    /* Allocate/initialize the memory for image information.  REQUIRED. */
+    png_infop info_ptr;
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL) {
+        fclose(fp);
+        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+        goto finalise;
+    }
+
+
+    png_init_io(png_ptr, fp);
+    png_read_png(png_ptr, info_ptr, NULL, NULL);
+
+    png_bytep *rows;
+    rows = png_get_rows(png_ptr, info_ptr);
+
+    for (int y = 0 ; y < height ; y++) {
+        for (int x = 0; x < width; x++) {
+            buffer[y * width * 4 + 4 * x + 0] = rows[y][4 * x + 0];
+            buffer[y * width * 4 + 4 * x + 1] = rows[y][4 * x + 1];
+            buffer[y * width * 4 + 4 * x + 2] = rows[y][4 * x + 2];
+            buffer[y * width * 4 + 4 * x + 3] = rows[y][4 * x + 3];
+        }
+    }
+
+finalise:
+    if (fp != NULL) fclose(fp);
+    if (png_ptr != NULL) png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+
+    return code;
 }
 
 int write_png(const char* filename, int width, int height, unsigned char *buffer, char* title)
